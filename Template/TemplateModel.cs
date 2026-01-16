1a403261750858e9540b626f3c7662e108f0867c
liangcheng <liangcheng@bigai.ai>
2026-01-16 09:44:58 +0800

    将tt转成cs

--- Template/TemplateModel.cs
+++ Template/TemplateModel.cs
@@ -2,88 +2,88 @@
 
 namespace protoc_gen_turbolink.Template
 {
-    partial class ClientCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ClientCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ClientH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ClientH(GrpcServiceFile _s, GenerateParam _g) {s = _s; g = _g; }
-    }
-    partial class ContextCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ContextCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ContextH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ContextH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class MarshalingCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public MarshalingCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class MarshalingH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public MarshalingH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class MessageH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public MessageH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class MessageCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public MessageCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ServiceCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ServiceCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ServiceH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ServiceH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ServicePrivateH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ServicePrivateH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class ServicePrivateCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public ServicePrivateCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class NodeH
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public NodeH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
-    partial class NodeCPP
-    {
-        private GrpcServiceFile s;
-        private GenerateParam g;
-        public NodeCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
-    }
+    // partial class ClientCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ClientCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ClientH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ClientH(GrpcServiceFile _s, GenerateParam _g) {s = _s; g = _g; }
+    // }
+    // partial class ContextCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ContextCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ContextH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ContextH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class MarshalingCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public MarshalingCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class MarshalingH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public MarshalingH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class MessageH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public MessageH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class MessageCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public MessageCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ServiceCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ServiceCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ServiceH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ServiceH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ServicePrivateH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ServicePrivateH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class ServicePrivateCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public ServicePrivateCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class NodeH
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public NodeH(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
+    // partial class NodeCPP
+    // {
+    //     private GrpcServiceFile s;
+    //     private GenerateParam g;
+    //     public NodeCPP(GrpcServiceFile _s, GenerateParam _g) { s = _s; g = _g; }
+    // }
 }
