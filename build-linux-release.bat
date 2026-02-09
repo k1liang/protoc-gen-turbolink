1bdbe94d40c049ca4d5a42e4c3d5eba0edcb0b2e
liangcheng <liangcheng@bigai.ai>
2026-02-09 15:50:15 +0800

    修改bat路径

--- build-linux-release.bat
+++ build-linux-release.bat
@@ -1,2 +1,2 @@
 dotnet publish protoc-gen-turbolink.csproj -c Release  -r linux-x64   --self-contained true   -o publish/linux
-copy publish\linux\protoc-gen-turbolink C:\Work\TongSim\tongtestue5\Plugins\TongosGrpc\GrpcLibraries\Linux /Y
+copy publish\linux\protoc-gen-turbolink ..\..\Plugins\TongosGrpc\GrpcLibraries\Linux /Y
